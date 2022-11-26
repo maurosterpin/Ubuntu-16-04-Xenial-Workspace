1fdcf1afc153cdd5b7eb0053c97906c95d0496ad
maurosterpin <msterpindev@gmail.com>
2022-11-26 09:48:40 +0000

    amcl.launch fix

--- devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
+++ devel/share/ball_chaser/cmake/ball_chaser-msg-extras.cmake
@@ -1,2 +1,2 @@
 set(ball_chaser_MESSAGE_FILES "")
-set(ball_chaser_SERVICE_FILES "/home/workspace/new_ws/src/ball_chaser/srv/DriveToTarget.srv")
+set(ball_chaser_SERVICE_FILES "/home/workspace/Where-Am-I/src/ball_chaser/srv/DriveToTarget.srv")
